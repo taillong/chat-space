ac110d60a52e77285d4ff7a72b93df4a7b44c9df
taillong <taillong610@gmail.com>
2020-04-24 12:39:55 +0900

    ビューの整理

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path notice: 'グループを編集しました'
+      redirect_to root_path, notice: 'グループを編集しました'
     else
       render :edit
     end
