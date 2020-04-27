a4492a77c64e1fb97f7dfb3622b7ea29d486d70f
taillong <taillong610@gmail.com>
2020-04-27 21:02:42 +0900

    終わったー

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    # config.action_view.field_error_proc = Proc.new{ |html_tag, instance| html_tag }
+    config.time_zone = 'Tokyo'
   end
 end
