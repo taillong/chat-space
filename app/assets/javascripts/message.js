7fa1c0c62e2228da2a3ae5512cd01f89134b0b05
taillong <taillong610@gmail.com>
2020-04-28 17:34:10 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,11 +37,10 @@ $(function(){
     };
   }
   
-  $('#new_message').on('submit',function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
-
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
